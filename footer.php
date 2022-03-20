a4109db53afdfc611467a36eba0f06d50590e06d
KingTigrou <matth76330@live.fr>
2022-03-20 20:27:07 +0100

    First Commit

---
+++ footer.php
@@ -0,0 +1,57 @@
+<!-- footer start-->
+
+<div id="footer" class="Address layout_padding">
+
+   <div class="menu_main">
+      <div class="menu_text">
+         <ul>
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="about.html">About</a></li>
+            <li><a href="testmonial.html">Testmonial</a></li>
+            <li><a href="clients.html">Shop</a></li>
+            <li><a href="contact.html">Contact Us</a></li>
+         </ul>
+      </div>
+   </div>
+</div>
+</div>
+
+<div id="plant" class="footer layout_padding">
+   <div class="container">
+      <p>© 2022 All Rights Reserved. Bet Training</a></p>
+   </div>
+</div>
+<!-- Javascript files-->
+<script src="js/jquery.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/jquery-3.0.0.min.js"></script>
+<script src="js/plugin.js"></script>
+<!-- sidebar -->
+<script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+<script src="js/custom.js"></script>
+<!-- javascript -->
+<script src="js/owl.carousel.js"></script>
+<script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
+<script>
+   $(document).ready(function() {
+      $(".fancybox").fancybox({
+         openEffect: "none",
+         closeEffect: "none"
+      });
+
+      $(".zoom").hover(function() {
+
+         $(this).addClass('transition');
+      }, function() {
+
+         $(this).removeClass('transition');
+      });
+   });
+</script>
+
+
+
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- basic -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- mobile metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+    <!-- site metas -->
+    <title><?php $Title ?></title>
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="author" content="Fontaine Matthieu">
+    <!-- bootstrap css -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- style css -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Responsive-->
+    <link rel="stylesheet" href="css/responsive.css">
+    <!-- fevicon -->
+    <link rel="icon" href="images/fevicon.png" type="image/gif" />
+    <!-- Scrollbar Custom CSS -->
+    <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
+    <!-- Tweaks for older IEs-->
+    <link rel="stylesheet" href="css/font-awesome.css">
+    <!-- owl stylesheets -->
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css" media="screen">
+</head>
+<!-- body -->
+
+<body class="main-layout about_pape">
+    <!-- loader 
+    <div class="loader_bg">
+        <div class="loader"><img src="images/loading.gif" alt="#" /></div>
+    </div>
+     end loader -->
+    <!-- header -->
+    <header class="section">
+        <!-- header inner -->
+        <div class="header_main">
+            <div class="header_main">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+                            <div class="full">
+                                <div class="center-desk">
+                                    <div class="logo"> <a href="index.html"><img src="images/logo.png" alt="#"></a> </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
+                            <div class="menu-area">
+                                <div class="limit-box">
+                                    <nav class="main-menu">
+                                        <ul class="menu-area-main">
+                                            <li class="last"><a href="#"><img src="images/search_icon.png" alt="icon" /></a></li>
+                                            <li> <a href="index.php">Jouer !</a> </li>
+                                            <li><a href="formations.php">Nos formations</a></li>
+                                            <li><a href="contact.php">Nous contacter</a></li>
+                                            <li><a href="connexion.php">Connexion / Inscription</a></li>
+                                        </ul>
+                                    </nav>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- end header inner -->
+    </header>
+    <!-- end header -->
